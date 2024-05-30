abfee459e4a53aaa83989d4dd60666e957a9960b
ihleung <ihfungleung@gmail.com>
2024-05-30 11:01:56 -0400

    updated getallreservation function

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -36,7 +36,7 @@ const getUserWithEmail = function(email) {
   }
   return Promise.resolve(resolvedUser);
   */
-  console.log(email);
+  //console.log(email);
   return pool.query(
     'SELECT users.* FROM users WHERE users.email = $1;',
     [email])
@@ -100,7 +100,15 @@ const addUser = function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  //return getAllProperties(null, 2);
+  return pool.query(
+    'SELECT reservations.*, properties.*, avg(rating) as average_rating FROM reservations JOIN properties ON reservations.property_id = properties.id JOIN property_reviews ON properties.id = property_reviews.property_id WHERE reservations.guest_id = $1 AND reservations.end_date < now()::date GROUP BY properties.id, reservations.id ORDER BY reservations.start_date LIMIT $2;', [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    }).catch((err) => {
+      console.log(err.message);
+    });
+  
 };
 
 /// Properties
